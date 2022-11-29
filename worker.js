2c7f37573ef8830f740943ccd06339674d105d34
evansjt <evansjt@rose-hulman.edu>
2022-11-29 00:34:36 -0500

    Fixed port bug in worker.

--- worker.js
+++ worker.js
@@ -1,14 +1,13 @@
 import express from "express";
 import { fetchMetaData } from "./updateDB.js";
 
-// const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
-// const app = express();
+const app = express();
 
-// app.listen(port, 
-(async () => {
+app.listen(port, async () => {
     console.log("Worker application listening.....");
     while (true) {
         await fetchMetaData();
     }
-})();
+});
