2adbc676296f77cf3d0b35cb5e9a75155d090f55
evansjt <evansjt@rose-hulman.edu>
2022-11-29 02:06:05 -0500

    Create 200 response from worker.

--- worker.js
+++ worker.js
@@ -5,6 +5,10 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 
+app.get('/', (req,res) => {
+    res.sendStatus(200);
+});
+
 app.listen(port, async () => {
     console.log("Worker application listening.....");
     while (true) {
