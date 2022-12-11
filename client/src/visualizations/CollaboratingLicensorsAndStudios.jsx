2423b9d7c3845c824d22051a965cce6853e4fa4e
evansjt <evansjt@rose-hulman.edu>
2022-12-11 03:33:39 -0500

    Changed heatmap grid color.

--- client/src/visualizations/CollaboratingLicensorsAndStudios.jsx
+++ client/src/visualizations/CollaboratingLicensorsAndStudios.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import createPlotlyComponent from 'react-plotly.js/factory';
 
 const Plot = createPlotlyComponent(Plotly);
-const layout = { title: { text: '<b>Frequencies of Collaborations between Anime Licensors and Studio</b><br><i style="font-size:12px">(Raw data can be seen with API extension: /collab-lics-studs)</i>', font: { size: 18 }, y: 0.95 }, xaxis: { title: '<b>Licensor</b>', font: { size: 14 }, side: 'top', tickangle: -45 }, yaxis: { title: '<b>Studio</b>', font: { size: 14 }, autorange: 'reversed', automargin: true }, height: 1200, margin: { t: 250, l: 250, r: 200 }, paper_bgcolor: '#E0E0E0', plot_bgcolor: '#E0E0E0' };
+const layout = { title: { text: '<b>Frequencies of Collaborations between Anime Licensors and Studio</b><br><i style="font-size:12px">(Raw data can be seen with API extension: /collab-lics-studs)</i>', font: { size: 18 }, y: 0.95 }, xaxis: { title: '<b>Licensor</b>', font: { size: 14 }, side: 'top', tickangle: -45 }, yaxis: { title: '<b>Studio</b>', font: { size: 14 }, autorange: 'reversed', automargin: true }, height: 1200, margin: { t: 250, l: 250, r: 200 }, paper_bgcolor: '#E0E0E0', plot_bgcolor: '#6f6f6f' };
 
 function CollaboratingLicensorsAndStudios() {
     const [data, setData] = useState([]);
